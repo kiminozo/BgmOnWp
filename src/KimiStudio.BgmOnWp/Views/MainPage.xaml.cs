6f168d9ce098e4c5e2b44c8fec04a583f230ea87
kiminozo <kiminozo.eien@gmail.com>
2012-02-26 13:33:00 +0800

    View

--- src/KimiStudio.BgmOnWp/Views/MainPage.xaml.cs
+++ src/KimiStudio.BgmOnWp/Views/MainPage.xaml.cs
@@ -1,14 +1,23 @@
 ﻿using System.Windows;
+using System.Windows.Media;
 using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
 
 namespace KimiStudio.BgmOnWp.Views
 {
-    public partial class MainPage 
+    public partial class MainPage
     {
+        private readonly Color backColor = Color.FromArgb(0, 0, 0, 0);
+
         // 构造函数
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        private void OnAppBarStateChanged(object sender, ApplicationBarStateChangedEventArgs e)
+        {
+            ApplicationBar.BackgroundColor = e.IsMenuVisible ? backColor : Colors.Transparent;
         }
     }
 }
